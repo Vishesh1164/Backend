d1d572cf331748b85ad6a86af5653bf50a04427e
Vishesh1164 <srivastavavishesh59@gmail.com>
2024-11-12 20:31:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Impoorting express 
+const express = require('express');
+
+// Creating n express app
+
+const app = express();
+
+const port =5000;
+
+// stating the server
+
+app.listen(port, ()=>{console.log('Server started')})
